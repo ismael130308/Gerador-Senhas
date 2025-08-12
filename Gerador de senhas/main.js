1d5ad154f9aba982dc280c0ab40fbedff569a746
ismael130308 <159185086+ismael130308@users.noreply.github.com>
2025-08-12 11:13:20 -0300

    11/12/12

---
+++ Gerador de senhas/main.js
@@ -0,0 +1,71 @@
+const numeroSenha = Document.querySelector('.parametro-senha_texto')
+let tamanhosenha = 12;
+numeroSenha.textContent =tamanhosenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwyz';
+const numeros = '0123456789';
+const simbulo = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+const campoSenha = document.querySelector('#campo-senha')
+const checkbox = document.querySelectorAll('.checkbox')
+const forcasenha = document.querySelectorAll('.forca')
+
+botoes[0].onclick = diminuitamanho;
+botoes[1].onclick = aumentatamanho;
+
+function diminuitamanho() {
+    if (tamanhosenha > 1) {
+        //tamanhosenha = tamanhosenha-1;
+        tamanhosenha--;
+
+    }
+    
+    numeroSenha.textContent = tamanhosenha;
+    gerasenha()
+}
+function aumentatamanho(){
+    if(tamanhosenha < 20){
+        //tamanhosenha = tamanhosnha+1;
+        tamanhosenha++;
+    }
+    numeroSenha.textContent = tamanhosenha;
+    gerasenha();
+}
+
+for (i = 0; i < checkbox.length; i++){
+checkbox[i].onclick = gerasenha;
+}
+
+gerasenha ();
+function gerasenha() {
+    let alfabeto ='';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+         alfabeto = alfabeto + numeros;
+     }
+    if (checkbox[3].checked) {
+         alfabeto = alfabeto + simbulo;
+     }
+     let senha =";
+     for (let i = 0; <tamanhosenha;i++) {
+     let numerosAleatorios = Math.random()*alfabeto.length;
+     numerosAleatorios = math.flor(numerosAleatorios);
+     senha = senha + alfabeto[numerosAleatorios];
+     }
+     campoSenha.value = senha;
+     classificaSenha(alfabeto.length)
+}
+function classificaSenha(tamanhoAlfabeto){
+ let entropia = tamanhosenha * Math.log2(tamanhosenha)
+ console.log(entropia);
+ forcasenha.classList.remove('fraca','media','fort');
+ if (entropia > 57){
+    forcasenha.classList.add('forte');
+
+ }
+}
